fbc367d55bbd8bfde7e779d909c5d00c438b2391
ALI <alirezaei.eng7@gmail.com>
2026-09-25 15:03:24 +0330

    complete mobile layout

--- js/main.js
+++ js/main.js
@@ -37,8 +37,16 @@ const scrollHeader = () => {
     ? header.classList.add("scroll-header")
     : header.classList.remove("scroll-header");
 };
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = $.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
