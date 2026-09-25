467598d3d0d1ba39680dbf0a19690f3592f9e5e7
ALI <alirezaei.eng7@gmail.com>
2026-09-25 18:05:33 +0330

    end layout

--- js/main.js
+++ js/main.js
@@ -77,3 +77,12 @@ const scrollActive = () => {
 
 window.addEventListener("scroll", scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 1500,
+  delay: 300,
+  easing: "cubic-bezier(0.34, 1.56, 0.64, 1)",
+});
+
+sr.reveal("home__title", { origin: "top" });
